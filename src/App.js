8227d2304992d5c32de74af1163f0f91507131e6
Adam Purcell <apurcell88@gmail.com>
2023-07-21 12:01:04 -0500

    Deleted Nav.js content. Re-doing Nav from scratch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
+import { useState } from 'react';
 import Header from './components/Nav';
+import Main from './components/Main';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { ReactComponent as Netflix } from 'C:/Users/apurc/Documents/codewell/navbar/src/Assets/Netflix Logo.svg';
 
 function App() {
+  // STATE MANAGEMENT
+  const [displayPopup, setDisplayPopup] = useState(false);
+
   return (
     <div className="App">
       <Header />
-      
+      <Main />
     </div>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,25 +4,10 @@ import Navbar from 'react-bootstrap/Navbar';
 
 import { ReactComponent as Logo } from 'C:/Users/apurc/Documents/codewell/navbar/src/Assets/Logo.svg';
 
-const Header = () => {
+const Header = (props) => {
   return (
-    <div>
-      <Navbar expand="lg" className="bg-body-tertiary">
-        <Container>
-          <Navbar.Brand href="#home">
-            <Logo />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Products</Nav.Link>
-              <Nav.Link href="#link">Challenges</Nav.Link>
-              <Nav.Link href="#link">Resources</Nav.Link>
-              <Nav.Link href="#link">Other Links</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+    <div className='nav--container'>
+      
     </div>
   );
 }
