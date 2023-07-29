bd077abccfd231cb118d9f2c28984ce8dd2ac8f2
Adam Purcell <apurcell88@gmail.com>
2023-07-29 14:55:36 -0500

    Styled for mobile.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,3 @@
-// import Container from 'react-bootstrap/Container';
-// import Nav from 'react-bootstrap/Nav';
-// import Navbar from 'react-bootstrap/Navbar';
-
 import { ReactComponent as Logo } from 'C:/Users/apurc/Documents/codewell/navbar/src/Assets/Logo.svg';
 
 const Nav = (props) => {
